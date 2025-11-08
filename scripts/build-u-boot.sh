784457651f986e5bd92af080564257f2621070da
Peyton Howe <peyton.howe@bellsouth.net>
2025-11-07 23:01:15 -0800

    Add build-u-boot script
    * Only supports rk3588 devices for now

---
+++ scripts/build-u-boot.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+set -eE 
+trap 'echo Error: in $0 on line $LINENO' ERR
+
+if [ "$(id -u)" -ne 0 ]; then
+    echo "Please run as root"
+    exit 1
+fi
+
+if [[ -z ${UBOOT_RULES_TARGET} ]]; then
+    echo "Error: UBOOT_CONFIG is not set"
+    # Source board-specific configuration
+    if [[ -f "${ROOT_DIR}/configs/boards/${BOARD}.sh" ]]; then
+        source "${ROOT_DIR}/configs/boards/${BOARD}.sh"
+    else
+        echo "Warning: No board config found for ${BOARD}"
+        exit
+    fi
+fi
+
+cd "$(dirname -- "$(readlink -f -- "$0")")" && cd ..
+mkdir -p build && cd build
+mkdir -p u-boot && cd u-boot
+
+git clone --depth=1 --progress https://source.denx.de/u-boot/u-boot.git
+git clone --depth=1 --progress https://github.com/rockchip-linux/rkbin.git
+
+cd u-boot
+make clean
+make CROSS_COMPILE=aarch64-linux-gnu- \
+     ROCKCHIP_TPL=../rkbin/bin/rk35/rk3588_ddr_lp4_2112MHz_lp5_2400MHz_v1.19.bin \
+     BL31=../rkbin/bin/rk35/rk3588_bl31_v1.51.elf \
+     ${UBOOT_RULES_TARGET} all -j${nproc}  
+
+cp idbloader.img ..
+cp u-boot.itb ..
+
